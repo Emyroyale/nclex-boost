8d1b71aa1f15cb124cd070e63cf3b42b0d192f43
Emyroyale <105296318+Emyroyale@users.noreply.github.com>
2025-08-26 22:06:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 window.WORKER_URL = "https://nclex-tutor.emalet07.workers.dev";
 
 // Supabase (fill in later if/when you enable it)
-window.SUPABASE_URL = "";
-window.SUPABASE_ANON_KEY = "";
+window.SUPABASE_URL = "https://awfhdinvmhalhdrecxla.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_secret_Yv4FxWo4plI-TV68V3dnow_lg8KaXfo";
